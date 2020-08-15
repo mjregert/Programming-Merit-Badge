01ee074cd2e9133a0f08d471a61387a96f48cae8
Michael J. Regert <codemonkey42@users.noreply.github.com>
2020-08-15 09:36:12 -0500

    Add notes, etc.

---
+++ sandbox/main.py
@@ -0,0 +1,53 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+#def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    #print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+#if __name__ == '__main__':
+#    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+from turtle import *
+# color('red', 'yellow')
+# begin_fill()
+# while True:
+#     forward(200)
+#     left(170)
+#     if abs(pos()) < 1:
+#         break
+# end_fill()
+
+
+from turtle import *
+pencolor('blue')
+goto(-100, 0)
+pendown()
+# House Frame
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+
+#door
+goto(-20, 0)
+left(90)
+forward(80)
+right(90)
+forward(40)
+right(90)
+forward(80)
+
+#roof
+goto
+done()
